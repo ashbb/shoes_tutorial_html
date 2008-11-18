095050cbaf357add1c0c7f36c4e7894d116f0a6e
ashbb <ashbbb@gmail.com>
2008-11-19 03:18:43 +0900

    excuted all samples with Shoes-0.r1091 and updated some .png files and edited some .mdown files

--- src/sample20.rb
+++ src/sample20.rb
@@ -2,7 +2,7 @@
 Shoes.app :title => 'Sorter', :width => 180, :height => 80 do
   background gradient powderblue, royalblue
   msg = para '', :size => 8
-  
+
   yar = image('../images/yar.png', :left => 60, :top => 18).click do
     self.clipboard = self.clipboard.sort unless self.clipboard.nil?
     yar.transform :center
