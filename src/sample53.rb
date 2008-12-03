6fe5ec7a619215665d7acc67cd13437f83e22839
ashbb <ashbbb@gmail.com>
2008-12-04 04:44:34 +0900

    added a new sample 53

---
+++ src/sample53.rb
@@ -0,0 +1,24 @@
+# sample53.rb
+class Shoes::Image
+  def small
+    self.style :width => self.width / 2, :height => self.height / 2
+  end
+  
+  def big
+    self.style :width => self.width * 2, :height => self.height * 2
+  end
+end
+
+PATH = '../images/yar.png'
+
+Shoes.app :width => 250, :height => 150 do
+  w, h = imagesize(PATH)
+  img = image PATH, :width => w, :height => h, :name => PATH.split('/').last
+  msg = para 'ready', :left => w, :top => h
+  every 3 do
+    img.style[:width] > w ? img.small : img.big
+    msg.text = "#{img.style[:name]} width is : #{img.style[:width]}" + "\n" +
+               "#{img.style[:name]} height is : #{img.style[:height]}"
+  end
+end
+  
