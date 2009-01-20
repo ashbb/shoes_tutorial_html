8f1c74da2142cc0f2f7374c494864b951903ac44
ashbb <ashbbb@gmail.com>
2009-01-21 02:06:50 +0900

    added sample 62

---
+++ src/sample62.rb
@@ -0,0 +1,35 @@
+# sample62.rb
+  # See the following original code.
+  # 05_interactivity_with_objects.rb
+  # http://github.com/karmi/shoes_demonstration_apps/tree/master
+
+class Letter < Widget
+  def initialize img
+    @flag = false
+    img.click{@flag = true; @img = img}
+    img.release{@flag = false}
+    motion{|left, top| @img.move(left-50, top-100) if @flag}
+  end
+end
+  
+Shoes.app :width => 800, :height => 600 do
+  sound = video 'assets/drumfill.aif', :width => 0, :height => 0
+  para 'Input Your Name: '
+  spell = edit_line
+  button 'GO' do
+    sound.play
+    @canvas.clear do
+      spell.text.downcase.split('').each_with_index do |l, i|
+        letter  image("letters/#{l}.jpg", :left => 100*(i%8), :top => 150*(i/8))
+      end
+    end
+  end
+  
+  @canvas = flow do
+    a = 'a'
+    26.times do |l|
+      letter image("letters/#{a}.jpg").move(rand(width), rand(height))
+      a.next!
+    end
+  end
+end
