8438f842044371fc1ebbc5e74cd7844f13bb4f18
ashbb <ashbbb@gmail.com>
2009-02-05 21:32:07 +0900

    added a link to Michael's blog site and a comment into gallery4.rb for BlueCloth

--- src/gallery4-1.rb
+++ src/gallery4-1.rb
@@ -5,7 +5,7 @@ agent = WWW::Mechanize.new
 page = ARGV[0]
 
 page_content = agent.get(page)
-image_regex = /<img src="(.*)" title="(.*)" alt="(.*)"/
+image_regex = /<img src="(.*)" title="(.*)" alt="(.*)"/  # > : for BlueCloth
 
 image_regex.match page_content.search("//img[contains(@src,'comics')]").to_s
 open('xkcd_tmp.viewer', 'w'){|f| f.puts $1, $2, $3}
